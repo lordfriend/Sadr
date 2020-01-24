57b0c27770d63803abd785705e1f205a1d31abc6
lordfriend <lordfriend@outlook.com>
2020-01-24 13:25:11 +0800

    add gitlab pipeline, adjust the build process

--- build.sh
+++ build.sh
@@ -9,9 +9,6 @@ zip -r chrome ./dist
 npm run build:prod:firefox
 npx web-ext build
 source ./sign-env.sh
-echo $WEB_EXT_API_KEY
-echo $WEB_EXT_API_SECRET
-echo $WEB_EXT_ID
 npx web-ext sign --api-key $WEB_EXT_API_KEY --api-secret $WEB_EXT_API_SECRET --channel unlisted --id $WEB_EXT_ID
 
 node ./scripts/ff-update-manifest.js
