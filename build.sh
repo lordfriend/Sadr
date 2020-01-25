c1d19d3b02b9e9c0aaa7728a55603e32173d6530
lordfriend <lordfriend@outlook.com>
2020-01-25 12:59:02 +0800

    update build.sh to see if ENV can applied

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ zip -r chrome ./dist
 
 # firefox
 npm run build:prod:firefox
+echo $WEB_EXT_API_KEY
+echo $WEB_EXT_API_SECRET
+echo $WEB_EXT_ID
 npx web-ext build
 npx web-ext sign --api-key $WEB_EXT_API_KEY --api-secret $WEB_EXT_API_SECRET --channel unlisted --id $WEB_EXT_ID
 
